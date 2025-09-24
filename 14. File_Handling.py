2cab4a24facc64073918fb1a10fd1c475f64fb65
chillnithin-debug <chillnithin@gmail.com>
2025-09-24 10:57:00 +0530

    Update 14. File_Handling.py

--- 14. File_Handling.py
+++ 14. File_Handling.py
@@ -2,4 +2,36 @@ file = open("student.txt","a")
 # write, writelines
 file.write("Hello c++\n")
 text = b"Hello Nandhan"
+
 file.close()
+# types of read():
+# 1. read() =>
+# file = open("pythin.txt","w")
+# content = file.read()
+# print(content)
+# file.close()
+# 2. readline() =>
+# file = open("student.txt","r")
+# content = file.readlines() 1 st line 
+# content1 = file.readlines() 2nd line 
+# content2 = file.readline() 3rd line
+# print (content)
+# print (content1)
+# print (content2)
+# file.close()
+# 3. readlines() =>
+# file = open("pythin.txt","w")
+# content = file.read()
+# print(content)
+# file.close()
+
+# file = open("python.txt","a")
+# write,writelines
+# file.write("hello c++\n")
+# file.close()
+
+file = open("python.txt","a")
+# write,writelines
+file.write("hello c++\n")
+file.close() 
+
